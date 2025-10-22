bc58193cffe2834879aa23b45f5989832c27f602
imDanyil <160357651+imDanyil@users.noreply.github.com>
2025-10-22 09:11:16 +0300

    Remove unused animation constants
    Removed unused pageVariants and pageTransition constants.

--- src/App.tsx
+++ src/App.tsx
@@ -5,27 +5,6 @@ import { AppRoutes } from "./configs/routesConfig";
 import Header from "./components/Header";
 import "./App.css";
 
-const pageVariants = {
-  initial: {
-    opacity: 0,
-    x: "-100vw",
-  },
-  in: {
-    opacity: 1,
-    x: 0,
-  },
-  out: {
-    opacity: 0,
-    x: "100vw",
-  },
-};
-
-const pageTransition = {
-  type: "tween",
-  ease: "anticipate",
-  duration: 0.5,
-};
-
 const AnimatedRoutes = () => {
   const location = useLocation();
   const isScrollablePage = ["/"].includes(location.pathname);
